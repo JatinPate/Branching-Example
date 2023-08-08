b62733a36eef9d1df4636aae1f9a58d73cf4df58
Jatin Patel <jatin@wecancodeit.org>
2023-08-08 09:41:58 -0400

    Added Review Model

---
+++ PizzaNearYouSln/PizzaNearYou/Models/ReviewerModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PizzaNearYou.Models
+{
+    public class ReviewerModel
+    {
+
+        [Key]
+        public int id { get; set; }
+    }
+}
